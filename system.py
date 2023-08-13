2fe86321d7d45ad312503e08cbaecf59251ffdf8
Peter Chervenski <spookey@abv.bg>
2023-08-13 12:50:31 +0300

    update

--- system.py
+++ system.py
@@ -940,9 +940,9 @@ def backtest_strategy_single(strategy, data, skip_train=1, skip_val=0, skip_test
             theiter = tqdm(range(1, len(data)))
     else:
         if quiet:
-            theiter = range(0, len(data)-1)
+            theiter = range(0, len(data))
         else:
-            theiter = tqdm(range(0, len(data)-1))
+            theiter = tqdm(range(0, len(data)))
     for idx in theiter:
         current_time = data.index[idx].time()
         if not data.daily:
@@ -960,8 +960,8 @@ def backtest_strategy_single(strategy, data, skip_train=1, skip_val=0, skip_test
         action, confidence = strategy.next(idx, data)
 
         if system.enter_on_close:
-            entry_price = data.iloc[idx]['Close']
-            exit_price = data.iloc[idx+1]['Close']
+            entry_price = data.iloc[idx-1]['Close']
+            exit_price = data.iloc[idx]['Close']
         else:
             entry_price = data.iloc[idx]['Open']
             exit_price = data.iloc[idx]['Close']
@@ -984,10 +984,10 @@ def backtest_strategy_single(strategy, data, skip_train=1, skip_val=0, skip_test
                 'pos': action,
                 'conf': confidence,
                 'shares': shares,
-                'entry_datetime': data.index[idx],
-                'exit_datetime': data.index[idx+1] if system.enter_on_close else data.index[idx],
-                'entry_bar': idx,
-                'exit_bar': idx+1 if system.enter_on_close else idx,
+                'entry_datetime': data.index[idx-1] if system.enter_on_close else data.index[idx],
+                'exit_datetime': data.index[idx],
+                'entry_bar': idx-1 if system.enter_on_close else idx,
+                'exit_bar': idx,
                 'entry_price': entry_price,
                 'exit_price': exit_price,
                 'profit': profit
